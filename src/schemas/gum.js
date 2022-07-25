be973b4ffc1470cdeac74af2a5f8b1b8f7c50316
SuperSonic <supersonic@livemail.tw>
2022-07-25 12:54:17 +0800

    Add doc_type

--- src/schemas/gum.js
+++ src/schemas/gum.js
@@ -7,6 +7,7 @@ const Schema = mongoose.Schema;
 module.exports = new Schema({
     content: String,
     author: String,
+    doc_type: String,
     created_at: Number,
     updated_at: Number,
 });
